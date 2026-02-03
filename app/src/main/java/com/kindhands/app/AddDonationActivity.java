10512cd0d044b72d3cb12292709d13888abb34ea
Siddhi Bhendekar <siddhibhendekar15@gmail.com>
2026-02-03 20:15:43 +0530

    .

--- app/src/main/java/com/kindhands/app/AddDonationActivity.java
+++ app/src/main/java/com/kindhands/app/AddDonationActivity.java
@@ -31,13 +31,13 @@ public class AddDonationActivity extends AppCompatActivity {
 
         tvRequirements = findViewById(R.id.tvOrgRequirements);
 
-        // Find CardViews
+       /* // Find CardViews
         View clothes = findViewById(R.id.cardClothes);
         View food = findViewById(R.id.cardFood);
         View books = findViewById(R.id.cardBooks);
         View medical = findViewById(R.id.cardMedical);
         View toys = findViewById(R.id.cardToys);
-        View stationery = findViewById(R.id.cardStationery);
+        View stationery = findViewById(R.id.cardStationery);*/
 
        /* // Set Click Listeners
         if (clothes != null) clothes.setOnClickListener(v -> openForm("clothes"));
